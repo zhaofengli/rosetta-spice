34abd6fb5831fa3179f9bb35ee3d0c8019763372
Zhaofeng Li <hello@zhaofeng.li>
2025-06-24 14:21:03 +0000

    rosetta: 24C5079e -> 25A5295e

--- rosetta.nix
+++ rosetta.nix
@@ -2,11 +2,11 @@
 let
   # https://swscan.apple.com/content/catalogs/others/index-rosettaupdateauto-1.sucatalog.gz
 
-  # Sequoia 15.2 Beta 3
-  version = "24C5079e";
+  # Tahoe 26.0 Developer Beta 2
+  version = "25A5295e";
   src = fetchurl {
-    url = "https://swcdn.apple.com/content/downloads/38/30/072-18030-A_FANJVJ91EE/eeicx7eyga9aqrh2b3uif0yxqw7rn8dfuw/RosettaUpdateAuto.pkg";
-    hash = "sha256-rmShRTbhbMf+mKsQiKMss8uspnudnPeuxukcqCDY2wc=";
+    url = "https://swcdn.apple.com/content/downloads/39/46/082-58036-A_WCOVJJIR7H/dmu2qq79qjdn85h8vd8hj09v26qlx5i7l4/RosettaUpdateAuto.pkg";
+    hash = "sha256-SUeiaEiDMokhkRHrOmkWz+wRr/C98GyRczYlq2xr7bY=";
   };
 
   drv = stdenv.mkDerivation {
