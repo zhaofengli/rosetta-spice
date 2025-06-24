82714072ee5c5ae53ca4df7e17d0b9851e282078
Zhaofeng Li <hello@zhaofeng.li>
2025-06-24 20:07:28 +0000

    nixos: Support running without the Rosetta virtiofs mount

--- nixos/default.nix
+++ nixos/default.nix
@@ -20,6 +20,19 @@ in {
         type = types.bool;
         default = false;
       };
+      hasRosettaMount = lib.mkOption {
+        description = lib.mdDoc ''
+          Whether a host Rosetta virtiofs mount is available.
+
+          The `rosetta` binary file in this virtiofs mount supports special ioctls
+          which Rosetta uses to talk to the hypervisor.
+          You can configure the mount with the `config.virtualisation.rosetta` options.
+
+          If disabled, the kernel must have the TSO patches.
+        '';
+        type = types.bool;
+        default = true;
+      };
       enableAot = lib.mkOption {
         description = lib.mdDoc ''
           Whether to enable AOT translation if available.
@@ -46,18 +59,62 @@ in {
   };
   config = lib.mkIf cfg.enable (lib.mkMerge [
     {
+      virtualisation.rosetta.enable = lib.mkDefault true;
+
       assertions = [
         {
-          assertion = config.virtualisation.rosetta.enable;
-          message = "virtualisation.rosetta-spice requires virtualisation.rosetta.enable";
+          assertion = config.virtualisation.rosetta.enable == cfg.hasRosettaMount;
+          message = ''
+            The value of `virtualisation.rosetta.enable` must be the same as ahat of
+            `virtualisation.rosetta-spice.hasRosettaMount`.
+
+            In order to use Rosetta without a host Rosetta virtiofs mount, you
+            must use a kernel which supports the `PR_GET_MEM_MODEL` (`0x6d4d444c`)
+            prctl. This is highly recommended so TSO can be enabled on a per-thread
+            basis.
+
+            You can get the patches here: <https://github.com/AsahiLinux/linux/tree/bits/220-tso>
+          '';
+        }
+        {
+          assertion = !cfg.hasRosettaMount -> (cfg.rosettaPkg != null);
+          message = ''
+            `virtualisation.rosetta-spice.rosettaPkg` must be set to use
+            `virtualisation.rosetta-spice.hasRosettaMount = false`.
+          '';
         }
       ];
+    }
 
-      virtualisation.rosetta.enable = lib.mkDefault true;
+    # Upstream Rosetta module
+    (lib.mkIf (config.virtualisation.rosetta.enable) {
       boot.binfmt.registrations.rosetta = {
         interpreter = lib.mkForce "${rosettaPath}/rosetta";
       };
-    }
+    })
+
+    # No upstream Rosetta
+    # Adapted from nixpkgs/nixos/modules/virtualisation/rosetta.nix
+    (lib.mkIf (!config.virtualisation.rosetta.enable) {
+      boot.binfmt.registrations.rosetta = {
+        interpreter = "${rosettaPath}/rosetta";
+
+        # The required flags for binfmt are documented by Apple:
+        # https://developer.apple.com/documentation/virtualization/running_intel_binaries_in_linux_vms_with_rosetta
+        magicOrExtension = ''\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x3e\x00'';
+        mask = ''\xff\xff\xff\xff\xff\xfe\xfe\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff'';
+        fixBinary = true;
+        matchCredentials = true;
+        preserveArgvZero = false;
+
+        # Remove the shell wrapper and call the runtime directly
+        wrapInterpreterInShell = false;
+      };
+
+      nix.settings = {
+        extra-platforms = [ "x86_64-linux" ];
+      };
+    })
 
     # Patch mounted Rosetta
     (lib.mkIf (cfg.rosettaPkg == null) {
