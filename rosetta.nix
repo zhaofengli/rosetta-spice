fb8337591aa10a746277b57e2d4469465eb6dcc7
Zhaofeng Li <hello@zhaofeng.li>
2025-09-13 12:21:39 +0000

    rosetta: 25A5295e -> 25A353

--- rosetta.nix
+++ rosetta.nix
@@ -1,25 +1,25 @@
-{ lib, stdenv, fetchurl, p7zip }:
+{ lib, stdenv, fetchurl, p7zip, cpio }:
 let
   # https://swscan.apple.com/content/catalogs/others/index-rosettaupdateauto-1.sucatalog.gz
 
-  # Tahoe 26.0 Developer Beta 2
-  version = "25A5295e";
+  # Tahoe 26.0 RC
+  version = "25A353";
   src = fetchurl {
-    url = "https://swcdn.apple.com/content/downloads/39/46/082-58036-A_WCOVJJIR7H/dmu2qq79qjdn85h8vd8hj09v26qlx5i7l4/RosettaUpdateAuto.pkg";
-    hash = "sha256-SUeiaEiDMokhkRHrOmkWz+wRr/C98GyRczYlq2xr7bY=";
+    url = "https://swcdn.apple.com/content/downloads/61/63/093-37146-A_S9OR0EVSU7/85gm90bhzh16fd705jb6sjc3unnywcu7xv/RosettaUpdateAuto.pkg";
+    hash = "sha256-vNKVnLIZmxGhaCQk3CRvcu8i22QawSbMlcUfiFx33Xk=";
   };
 
   drv = stdenv.mkDerivation {
     pname = "rosetta";
     inherit version src;
 
-    nativeBuildInputs = [ p7zip ];
+    nativeBuildInputs = [ p7zip cpio ];
 
     unpackPhase = ''
       runHook preUnpack
 
       7z x $src
-      7z x Payload\~
+      cpio -idmv <Payload\~
 
       runHook postUnpack
     '';
